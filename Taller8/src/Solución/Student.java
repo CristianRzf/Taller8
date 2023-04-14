70c784af9c6808eb900e3c3aa742158581785661
CristianRzf <125093670+CristianRzf@users.noreply.github.com>
2023-04-14 16:44:37 -0500

    Taller 8

--- Taller8/src/Solución/Student.java
+++ Taller8/src/Solución/Student.java
@@ -4,22 +4,28 @@ public class Student {
 	String nombre;
 	int credits, qualityPoints;
 	double gpa;
-	Student(String nombre, int credits, int qualityPoints){
-		this.nombre=nombre;
-		this.credits=credits;
-		this.qualityPoints=qualityPoints;
-		gpa = (double)qualityPoints/(double)credits;
+
+	Student(String nombre, int credits, int qualityPoints) {
+		this.nombre = nombre;
+		this.credits = credits;
+		this.qualityPoints = qualityPoints;
+		gpa = (double) qualityPoints / (double) credits;
 	}
+
 	public double getGPA() {
 		return gpa;
 	}
+
 	public Student(int credits, int qualityPoints) {
-		this.credits+=credits;
-		this.qualityPoints+=qualityPoints;
-		gpa = (double)qualityPoints/(double)credits;
-		}
-	Student Mary=new Student("Mary Jones",14,46);
-	Student John=new Student("Mary Jones",60,173);
-	Student Ari=new Student("Mary Jones",31,69);
+		this.credits += credits;
+		this.qualityPoints += qualityPoints;
+		gpa = (double) qualityPoints / (double) credits;
+	}
+
+	Student Mary = new Student("Mary Jones", 14, 46);
+	Student John = new Student("Mary Jones", 60, 173);
+	Student Ari = new Student("Mary Jones", 31, 69);
 	
+	ari.addCredits(13,52);
+
 }
